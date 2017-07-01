7ee24575231dc1b21778336e0103ec1aae5b9536
rlichiere <rlichiere@visiativ.com>
2017-07-02 01:20:13 +0200

    improve config doc

--- catalog/core/config.py
+++ catalog/core/config.py
@@ -1,13 +1,14 @@
 try:
     import config_private
-
-    # Password used to initialize users.
-    # Value is encrypted. To initialize this field, you musthave to copy it from dbFor example
     DEFAULT_USER_PASSWORD = config_private.DEFAULT_USER_PASSWORD
-
 except Exception as e:
     print('private config not found. use default values')
 
+    # Create you own config_private file, or modify parameters below :
+
+    # ################################################ DEFAULT_USER_PASSWORD ###
+    # Used to initialize users password.
+    # Value is encrypted. To initialize this field, you can copy one from db.
     DEFAULT_USER_PASSWORD = 'pbkdf2_sha256$24000$...='
 
 # Project status are used in the project's workflow.
