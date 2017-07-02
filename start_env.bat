b138d77c2a6b42359106718d7942b3da95274a26
rlichiere <rlichiere@visiativ.com>
2017-07-02 23:47:08 +0200

    Participant: ui improvements Launcher improvement : dynamic port

--- start_env.bat
+++ start_env.bat
@@ -1,1 +1,21 @@
-python manage.py runserver 0.0.0.0:8099
+@echo off
+
+:parse
+IF "%~1"=="" (
+   GOTO param_missing
+) ELSE (
+   GOTO launch
+)
+
+:launch
+python manage.py runserver 0.0.0.0:%~1
+GOTO end
+
+:param_missing
+echo Missing parameter : port
+GOTO end
+
+:end
+echo End
+
+pause
